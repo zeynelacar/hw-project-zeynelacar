4e28c7d3c30e07c4632d5db8b22a8d345dd783ac
Zeynel <acarz16@itu.edu.tr>
2022-02-14 15:55:13 +0300

    me configurations.

--- src/main/java/com/example/project/model/Flat.java
+++ src/main/java/com/example/project/model/Flat.java
@@ -1,8 +1,19 @@
 package com.example.project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "blocks")
 public class Flat {
 
 
