a4a83297c702dc1e8170532f7079ef5e1ff11276
Zeynel <79790351+zeynelacar@users.noreply.github.com>
2022-02-20 01:14:21 +0300

    Deleted unused directory.
    Because of a capital letter error main directory had one unused directory.

--- src/main/java/com/example/Project/repository/BlockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.repository;
-
-import com.example.project.model.Block;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlockRepository extends JpaRepository<Block,Integer> {
-}
